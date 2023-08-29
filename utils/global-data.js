eac07ab8f8d2a4e67cf74457c458f6a90f348e68
Clara Santillan <94200508+Clarisantillan@users.noreply.github.com>
2023-08-29 15:12:16 -0300

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,17 +1,30 @@
+import React from 'react';
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Clara Santillan';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Bienvenidos a mi portfolio';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
-
+    : 'Todos los derechos reservados.';
+  const WhatsAppLink = () => {
+  const phoneNumber = '542243414876'; 
+  const message = 'Hola';
+  const encodedMessage = encodeURIComponent(message);
+  const whatsappLink = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodedMessage}`;
+  return (
+    <a href={whatsappLink} target="_blank" rel="noopener noreferrer">
+      Enviar mensaje por WhatsApp
+    </a>
+  );
+};
   return {
     name,
     blogTitle,
     footerText,
+    WhatsAppLink,
+    
   };
 };
